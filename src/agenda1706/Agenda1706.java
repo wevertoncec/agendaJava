3d0b5b05628ea6b92fdc8d72518407d9756db03b
weverton <weverton_apc21@hotmail.com>
2020-04-27 22:38:25 -0300

    comit inicial

---
+++ src/agenda1706/Agenda1706.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda1706;
+
+/**
+ *
+ * @author ALUNO
+ */
+public class Agenda1706 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/agenda1706/sobre.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda1706;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Barraco
+ */
+public class sobre {
+    public static void main(String srgs[]){
+        JOptionPane.showMessageDialog(null,"Desenvolvido por Weverton Versao 1.1");
+    }
+    
+}
